614d6a8c83e5bfe787149fe5f4d6c228d7a76f8a
udrepo <33026225+udrepo@users.noreply.github.com>
2021-01-06 23:02:27 +0600

    Update Post.java

--- src/main/java/com/Ulan/Jeli/entity/Post.java
+++ src/main/java/com/Ulan/Jeli/entity/Post.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "posts")
-public class Post extends AuditModel {
+public class Post{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
